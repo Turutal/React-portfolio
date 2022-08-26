bc03e3035907bb5f866015bc51f8313be68d9678
Turutal <76479081+Turutal@users.noreply.github.com>
2022-08-26 10:18:25 +0600

    Update portfolio.js
    Update portfolio

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,22 +1,19 @@
 import './style.scss';
 
-function Portfolio() {
-  const list = [
+const list = [
     { img: '/img/portfolio/shop.jpg', name: 'Online fashion store - Homepage' },
     { img: '/img/portfolio/reebok.jpg', name: 'Reebok Store - Concept' },
     { img: '/img/portfolio/braun.jpg', name: 'Braun Landing Page - Concept' },
   ];
 
-  const block = list.map((item) => {
-    return (
-      <div className="portfolio__item">
-        <img src={item.img} alt="shop" />
-        <a href="url">{item.name}</a>
-      </div>
-    );
-  });
-
-  return block;
+function Portfolio( {list} ) {
+  
+  return list.map((item) => (
+    <div className="portfolio__item">
+      <img src={item.img} alt="shop" />
+      <a href="url">{item.name}</a>
+    </div>
+  ));
 }
 
 export default Portfolio;
